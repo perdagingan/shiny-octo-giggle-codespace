a1be12340f6f2e0261bc7493edd3c87472ffe63d
Anam Fathonaya <050109245@ecampus.ut.ac.id>
2023-09-27 07:54:01 +0000

    Sebuah perubahan di package.json dan App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Anam Fathonaya",
+  title: "Back End Developer",
+  email: "kubiasaja@outlook.com",
+  gitHub: "perdagingan",
+  instagram: "kubiasaja",
+  linkedIn: "Anam Fathonaya",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "kubiasaja",
+  youTube: "InternetUser399",
 };
 
 const primaryColor = "#4E567E";
